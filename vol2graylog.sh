ed31dfb12915ce9c515940c6da10a51e02cbda1b
Megan Roddie <megansroddie@gmail.com>
2019-07-29 13:26:44 -0500

    new volatility plugin support

--- vol2graylog.sh
+++ vol2graylog.sh
@@ -55,7 +55,7 @@ volKDBG=${addrKDBG%L}
 
 # set array of plugins to be run
 
-declare -a plugins=("hashdump" "autoruns" "hivelist" "psscan" "handles" "netscan" "ldrmodules" "psxview" "modscan" "apihooks" "cmdscan" "consoles" "svcscan" "filescan")
+declare -a plugins=("malfind" "connscan" "pslist" "netscan" "dlllist" "getsids" "userassist" "psscan" "ldrmodules" "psxview" "modscan" "apihooks" "svcscan")
 
 # run all plugins listed in the plugins array and send results to graylogs
 
